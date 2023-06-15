3a6d44603d38275cdf3c0af3772b17d81e124f70
agtshm <chershiong@gmail.com>
2023-06-15 16:08:25 +0100

    update recruitment stats

--- components/RecruitmentStatsRow.js
+++ components/RecruitmentStatsRow.js
@@ -6,9 +6,9 @@ export default function RecruitmentStatsRow({
   const formatted_percent =
     parseFloat((number_of_participants / base_width) * 100).toFixed(2) + "%";
   return (
-    <div className="grid grid-cols-6 items-center">
-      <div className="col-span-2 text-sm">{center}</div>
-      <div className="col-span-3">
+    <div className="grid grid-cols-6 items-center mb-1">
+      <div className="col-span-3 text-sm">{center}</div>
+      <div className="col-span-2">
         <div className="w-full bg-white rounded-full h-2">
           <div
             className="bg-green-700 h-2 rounded-full"
