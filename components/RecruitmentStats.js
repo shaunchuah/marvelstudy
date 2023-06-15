46797b974e4d9597af8e91b33221e0fe40cc1722
Shaun Chuah <chershiong@gmail.com>
2023-06-15 15:07:06 +0100

    restructure and simplify recruitment stats

--- components/RecruitmentStats.js
+++ components/RecruitmentStats.js
@@ -2,20 +2,24 @@ import RecruitmentStatsData from "../data/recruitment_stats.json";
 import RecruitmentStatsRow from "./RecruitmentStatsRow";
 
 export default function RecruitmentStats() {
-  // import data from file here
-  const participant_number =
-    RecruitmentStatsData.current_number_of_participants;
+  // import data from file here and define constants
   const center_statistics = RecruitmentStatsData.center_statistics;
-  // end data import
-  var sorted_center_statistics = center_statistics.sort((a,b) => b.number_of_participants - a.number_of_participants);
-
-  const participant_percent =
-    RecruitmentStatsData.current_number_of_participants / 206;
-  const formatted_participant_percent =
-    parseFloat(participant_percent * 100).toFixed(2) + "%";
-  const last_updated = RecruitmentStatsData.last_updated;
-
   const base_width = 30; // use this number to adjust the denominator for the center breakdown bars
+  const target_participants = 206;
+
+  //perform data calculations and sorting
+  var sorted_center_statistics = center_statistics.sort(
+    (a, b) => b.number_of_participants - a.number_of_participants
+  );
+  let participant_number = 0;
+  sorted_center_statistics.forEach((center) => {
+    participant_number += center.number_of_participants;
+  });
+
+  const formatted_participant_percent =
+    parseFloat((participant_number / target_participants) * 100).toFixed(2) +
+    "%";
+  const last_updated = RecruitmentStatsData.last_updated;
 
   return (
     <div className="bg-white border p-6 rounded-lg  mb-4">
@@ -30,9 +34,9 @@ export default function RecruitmentStats() {
         <div>{participant_number} / 206 participants</div>
         <div className="text-sm italic">Last updated: {last_updated}</div>
       </div>
+
       <div className="mt-4">
         <h4 className="text-lg mb-2">Center Breakdown</h4>
-
         {sorted_center_statistics.map(({ center, number_of_participants }) => (
           <>
             <RecruitmentStatsRow
